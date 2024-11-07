e071bdd1fe78fd4e82bcdcb2e601915bd25103de
andersonalexsandro <anderson.silva.04@outlook.com>
2024-11-07 01:04:54 -0300

    bugfix: screen

--- src/screen.js
+++ src/screen.js
@@ -22,54 +22,61 @@ class Screen {
     }
 
     setUint8(address, value) {
-        switch(address){
+        switch(address) {
             case ScreenMap.X:
-                this.RAM.setUint8(address, value)
-                return
+                this.RAM.setUint8(address, value);
+                break;
 
             case ScreenMap.Y:
-                this.RAM.setUint8(address, value)
-                return
+                this.RAM.setUint8(address, value);
+                break;
 
             case ScreenMap.DRAW:
                 const pointDraw = this.getPointerIndex();
-                this.buffer[pointDraw] = 0b11111111
-                return
+                this.buffer[pointDraw] = 0b11111111;
+                break;
 
             case ScreenMap.CLEAR:
                 const pointClear = this.getPointerIndex();
-                this.buffer[pointClear] = ScreenMap.CLEAR_COMMAND
-                return
+                this.buffer[pointClear] = ScreenMap.CLEAR_COMMAND;
+                break;
 
             case ScreenMap.BUFFER_PUSH:
-                for (i = 0; i < this.buffer.length(); i++){
-                    const bufferContent = this.buffer[i]
+                for (let i = 0; i < this.buffer.length; i++) {
+                    const bufferContent = this.buffer[i];
 
-                    if (bufferContent === 0b00000000) continue
+                    if (bufferContent === 0b00000000) continue;
 
-                    if (bufferContent === 0b11111111){
-                        this.screen[i] = bufferContent
-                        continue
+                    if (bufferContent === 0b11111111) {
+                        this.screen[i] = bufferContent;
+                        continue;
                     } 
 
-                    if (bufferContent === ScreenMap.CLEAR_COMMAND){
-                        this.screen[i] = 0b00000000
-                        continue
+                    if (bufferContent === ScreenMap.CLEAR_COMMAND) {
+                        this.screen[i] = 0b00000000;
+                        continue;
                     } 
-                    
-                    throw new Error("Not a valid Bufferstate: " + bufferContent)
+
+                    throw new Error("Screen - Not a valid Buffer state: " + bufferContent);
                 }
+                this.buffer = new Uint8Array(this.screen.length);
+                break;
 
-            case ScreenMap.BUFFER_PUSH:
+            case ScreenMap.CLEAR_BUFFER:
+                this.buffer = new Uint8Array(this.screen.length);
+                break;
+
+            default:
+                throw new Error("Screen - Invalid address: " + address);
 
         }
     }
 
-    getPointerIndex(){
+    getPointerIndex() {
         const x = this.getX();
         const y = this.getY();
-        const point = (y * 32) + x
-        return point
+        const point = (y * 32) + x;
+        return point;
     }
 }
 
@@ -82,6 +89,6 @@ const ScreenMap = {
     BUFFER_PUSH: 0b00000101,
     CLEAR_BUFFER: 0b00000110,
     CLEAR_COMMAND: 0b10101010
-}
+};
 
 module.exports = Screen;
