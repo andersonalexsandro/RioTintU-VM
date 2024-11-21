6572a4760129ed36ce5049c3a66a39734bf49a05
andersonalexsandro <anderson.silva.04@outlook.com>
2024-11-21 19:22:18 -0300

    number display represetation

--- src/numberDisplay.js
+++ src/numberDisplay.js
@@ -1,11 +1,19 @@
 class NumberDisplay {
     constructor(RAMab) {
     this.RAM = new DataView(RAMab, 252, 2)
-    this.display = new Uint16Array(1)
     }
 
     setUint8(address, value) {
         this.RAM.setUint8(address, value)
+    }
+
+    getUint8(address) {
+        return 0b00000000
+    }
+
+    log() {
+        const decimalValue = parseInt(this.RAM.getUint8(0), 2); // Converte de binário para decimal
+        console.log(`Display: ${decimalValue}`);
     }
 }
 
