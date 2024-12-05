a5d386a2b7575174423aefa7a4d52c7aab6f023e
andersonalexsandro <anderson.silva.04@outlook.com>
2024-12-05 11:37:11 -0300

    number display done

--- ts/src/numberDisplay.ts
+++ ts/src/numberDisplay.ts
@@ -2,19 +2,28 @@ import Ram from "./ram";
 
 class NumberDisplay implements Memory{
 
+    public static readonly nBytesAlocated = 2; //16 bits to display
+
     private ram: Ram;
-    private display: Uint8Array;
+    private display: DataView;
+    private ramAlocated: DataView;
 
-    constructor(ram: Ram){
+    constructor(ram: Ram, initialAddress: number){
         this.ram = ram;
-        this.display = new Uint8Array(2);        
+        this.ramAlocated = new DataView(this.ram.getArrayBuffer(), initialAddress, NumberDisplay.nBytesAlocated);
+        this.display = new DataView(this.ramAlocated.buffer);        
     }
 
-    setValue(addres: number, value: number): void{
-
+    public setValue(addres: number, value: number): void{
+        this.ramAlocated.setUint8(addres, value);
     }
 
-    getValue(addres: number): number{
+    public getValue(addres: number): number{
         return 0;
     }
+
+    public toString(): string{
+        return("Display: " + this.display.getUint16(0, true)) //Little-endian true: low 8bits are in the first index
+    }
+
 }
