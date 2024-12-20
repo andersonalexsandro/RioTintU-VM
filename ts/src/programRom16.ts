c6d6796121fcce80a4a4f0271d513a90d365e441
andersonalexsanndro <anderson.silva.04@outlook.com>
2024-12-20 12:29:57 -0300

    memory mapper

--- ts/src/programRom16.ts
+++ ts/src/programRom16.ts
@@ -1,31 +1,31 @@
 export default class ProgramRom16{
 
-    private arrayBuffer: ArrayBuffer; // Pure array of Bytes, dont has methods to deal with
-    private dataView: DataView;  // Visualize and manipulate ArrayBuffer
+    private romAb: ArrayBuffer; // Pure array of Bytes, dont has methods to deal with
+    private rom: DataView;  // Visualize and manipulate ArrayBuffer
     private lengthInBytes: number;
 
-    constructor(nBytes: number){
-        this.lengthInBytes = nBytes;
-        this.arrayBuffer = new ArrayBuffer(nBytes);
-        this.dataView = new DataView(this.arrayBuffer);
+    constructor(lengthInBytes: number){
+        this.lengthInBytes = lengthInBytes;
+        this.romAb = new ArrayBuffer(lengthInBytes);
+        this.rom = new DataView(this.romAb);
     }
 
     public get16(address: number): number {
-        return this.dataView.getUint16(2 * address);
+        return this.rom.getUint16(2 * address);
     }
 
     public set16(address: number, value: number): void {
-        this.dataView.setUint16(address * 2, value);
+        this.rom.setUint16(address * 2, value);
     }
     
     public setHighLowBits(address: number, high8: number, low8: number): void {
-        this.dataView.setUint8(address, high8);
-        this.dataView.setUint8(address + 1, low8)
+        this.rom.setUint8(address * 2, high8);
+        this.rom.setUint8(address * 2 + 1, low8)
     }
 
     public toString(): string{
         let toString: string = '';
-        for(let i=0; i< this.lengthInBytes; i+=2){
+        for(let i=0; i< this.lengthInBytes; i++){
             toString+= `${i}: ${this.get16(i)}\n`
         } 
         return toString;
