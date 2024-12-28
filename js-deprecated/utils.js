d2ae6a2004344717ef52ef1165331c7c9c612025
andersonalexsandro <anderson.silva.04@outlook.com>
2024-12-28 15:58:57 -0300

    deleting js files

--- js-deprecated/utils.js
+++
@@ -1,8 +0,0 @@
-const concat4bits = (A, B) => {
-    if (A < 0 || A > 0b1111 || B < 0 || B > 0b1111) {
-        throw new Error('Os valores devem ser de 4 bits (0 a 15): A: ' + A + " B: " + B);
-    }
-    return (A << 4) | B;
-}
-
-module.exports = concat4bits;
