9a83c62c07a1db3a401952187fabe7f5f35ea375
andersonalexsandro <anderson.silva.04@outlook.com>
2024-12-30 20:57:43 -0300

    RioTintU init method

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -9,41 +9,47 @@ import { Registers } from "./registers";
 import Screen from "./screen";
 import { Assembler } from "./assembler/assembler";
 
-const ramLength = 256;
-const screenStart = 246;
-const screenWidth = 32;
-const screenHeigth = 32;
-const numberDisplayStart = 252;
-const romLength = 512;
-const registersLength = 16;
+export function RioTintUInit() {
+    const ramLength = 256;
+    const screenStart = 246;
+    const screenWidth = 32;
+    const screenHeigth = 32;
+    const numberDisplayStart = 252;
+    const romLength = 512;
+    const registersLength = 16;
+    
+    const rom = new ProgramRom16(romLength);
+    const registers = new Registers(registersLength);
+    registers.setRegisterNames(['r0', 'r1', 'r2', 'r3', 'r4', 'r5,', 'r6', 'r7', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15']);
+    const flags = new Flags();
+    const pc = new ProgramCounter8();
+    const ram = new Ram(ramLength);
+    const screen = new Screen(ram, screenStart, screenWidth, screenHeigth);
+    const numberDisplay = new NumberDisplay(ram, numberDisplayStart);
+    
+    const memoryMapper = new MemoryMapper();
+    memoryMapper.map(ram, 0, screenStart - 1, false);
+    memoryMapper.map(screen, screenStart, screenStart + Screen.nBytesAlocated - 1, true);
+    memoryMapper.map(numberDisplay, numberDisplayStart, numberDisplayStart + NumberDisplay.nBytesAlocated - 1, true);
+    
+    const cpu = new CPU(memoryMapper, rom, registers, flags, pc);
+    const assembler = new Assembler('./src/assembler/assembly', './src/assembler/assembled');
+    assembler.assembleFiles();
+    
+    return {
+        cpu,
+        ram,
+        rom,
+        registers,
+        flags,
+        pc,
+        numberDisplay,
+        screen,
+        memoryMapper,
+    };
+}
 
-let cpu: CPU;
-let ram: Ram;
-let rom: ProgramRom16;
-let registers: Registers;
-let flags: Flags;
-let pc: ProgramCounter8;
-let numberDisplay: NumberDisplay;
-let screen: Screen;
-let memoryMapper: MemoryMapper;
+RioTintUInit();
 
-rom = new ProgramRom16(romLength);
-registers = new Registers(registersLength);
-registers.setRegisterNames(['r0', 'r1', 'r2', 'r3', 'r4', 'r5,', 'r6', 'r7', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15'])
-flags = new Flags();
-pc = new ProgramCounter8();
-ram = new Ram(ramLength);
 
-screen = new Screen(ram, screenStart, screenWidth, screenHeigth);
-numberDisplay = new NumberDisplay(ram, numberDisplayStart);
-
-memoryMapper = new MemoryMapper();
-memoryMapper.map(ram, 0, screenStart - 1, false);
-memoryMapper.map(screen, screenStart, screenStart + Screen.nBytesAlocated - 1, true);
-memoryMapper.map(numberDisplay, numberDisplayStart, numberDisplayStart + NumberDisplay.nBytesAlocated - 1, true);
-
-cpu = new CPU(memoryMapper, rom, registers, flags, pc);
-
-const assembler = new Assembler('./src/assembler/assembly', './src/assembler/assembled');
-assembler.assembleFiles();
 
